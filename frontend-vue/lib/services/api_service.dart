41e52137fec7219ac1e8176474d62deaa611d86a
YadaSangphech6631501030 <6631501030@lamduan.mfu.ac.th>
2026-06-25 13:54:51 +0700

    Use localhost for API base URLs

--- frontend-vue/lib/services/api_service.dart
+++ frontend-vue/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = "http://192.168.110.150:5001";
+  static const String baseUrl = "http://localhost:5001";
   static const Duration _requestTimeout = Duration(seconds: 10);
 
   // ===== COMMON FUNCTION =====
